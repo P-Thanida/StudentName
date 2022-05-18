bc3976296f15182086ca09a820e876afbea47895
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-18 22:49:20 +0700

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
-   <form @submit="onSubmit" >
+   <form v-on:submit="submit">
     <div>
-      <label>Name : </label>
-      <input type="text" v-model="name" name="name" placeholder="Enter Your Name" />
+      <label for="name"> Name : </label>
+      <input type="text" v-model="applicant.name" name="name"  placeholder="Enter Your Name" />
     </div> <br>
+
     <div>
       <label>Age : </label>
-      <input
-        type="text"
-        v-model="age"
-        name="age"
-        placeholder="Enter Your Age"
-      />
+      <input type="text" v-model="applicant.age" name="age" placeholder="Enter Your Age"/>
     </div> <br>
-    <input type="submit" value="Save Information"/>
+    <button type="button" @click="clear">Clear</button>
+    <button type="button">Submit</button>
+    
   </form>
+   <div class="spy">#Spy {{JSON.stringify(applicant)}}</div>
 </template>
 
 <script>
 
 export default {
   name: "App",
-  // components: {
-  //   HelloWorld
-  // }
+  data(){
+    return {
+      applicant:{
+        name: "",
+        age: ""
+      }
+    };
+  },
+  methods:{
+    submit(event){
+      event.preventDefault();
+      alert(JSON.stringify(this.applicant))
+    },
+    clear(){
+      this.applicant ={
+        name: "",
+        age: ""
+      }
+    }
+  }
 }
 
 </script>
 <style>
+.spy{
+  margin-top: 16px;
+  color: red;
+}
 </style>
