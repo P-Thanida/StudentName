30b6cc0387e88583bf413123094df38d37d21dff
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-19 10:36:55 +0700

    Add Page

---
+++ public/main.js
@@ -0,0 +1,30 @@
+
+
+    new Vue ({
+        el: '#datalist',
+            data:{
+            title: 'data',
+                datas: [
+                    { name: 'Thanida Wichaidit'},
+                    { name: 'Apichaya Chuenjit'},
+                    { name: 'Nutcha Puttacoat'},
+                ]
+            },
+        
+        methods:{
+            newItem: function() {
+                if(!this.datas.name){
+                    return
+                }
+                this.datas.push({ 
+                    name:this.datas.name,
+                    del: ''
+                });
+                this.datas.name ="";        
+            },
+            delItem: function(data){
+                this.datas.splice(this.datas.indexOf(data),1)
+            }
+        }
+    
+    })
--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,19 @@
 <template>
-   <form v-on:submit="submit">
-    <div>
-      <label for="name"> Name : </label>
-      <input type="text" v-model="applicant.name" name="name"  placeholder="Enter Your Name" />
-    </div> <br>
-
-    <div>
-      <label>Age : </label>
-      <input type="text" v-model="applicant.age" name="age" placeholder="Enter Your Age"/>
-    </div> <br>
-    <button type="button" @click="clear">Clear</button>
-    <button type="button">Submit</button>
-    
-  </form>
-   <div class="spy">#Spy {{JSON.stringify(applicant)}}</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  data(){
-    return {
-      applicant:{
-        name: "",
-        age: ""
-      }
-    };
-  },
-  methods:{
-    submit(event){
-      event.preventDefault();
-      alert(JSON.stringify(this.applicant))
-    },
-    clear(){
-      this.applicant ={
-        name: "",
-        age: ""
-      }
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
-
 </script>
+
 <style>
-.spy{
-  margin-top: 16px;
-  color: red;
-}
+
 </style>
